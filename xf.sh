e7534e93a64a5bbe3e684de1f56efc324ea2b420
osliiw1 <102523856+osliiw1@users.noreply.github.com>
2022-03-29 22:17:12 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -69,7 +69,7 @@ cat <<EOF >/etc/v2ray/config.json
             "streamSettings": {
                 "network": "ws"
                 "wsSettings": {
-                "path": "/xf"
+                "path": "/"
                }
             }
         }
